bce5483af0f87e9fd59a227549e219025951185f
Naresh Chaudhary <nareshchaudhary1532@gmail.com>
2026-09-15 10:00:51 +0530

    create_kms_key= True

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -5,12 +5,12 @@ module "eks" {
   cluster_name    = "enterprise-eks-cluster"
   cluster_version = "1.28"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
   cluster_endpoint_public_access = true
 
-  # Conflict Fix: Disabling KMS and CloudWatch log duplicate creation
-  create_kms_key              = false
+  create_kms_key              = true
   create_cloudwatch_log_group = false
 
   eks_managed_node_groups = {
